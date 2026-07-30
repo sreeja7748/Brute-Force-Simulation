d9abb2ca99967c12424a236909c3f81f8d9a315e
sreeja7748 <sreeja77428@gmail.com>
2026-07-30 12:02:17 +0530

    Completed app.py

--- app.py
+++ app.py
@@ -2,8 +2,54 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
+# Fixed admin username and password
+admin_username = "admin"
+admin_password = "adminpassword"
 
-admin_username = 'admin'
-admin_password = 'adminpassword'
-
+# Dummy user data for registration
 users = {}
+
+@app.route("/")
+def home():
+    return render_template("product.html")
+
+
+
+@app.route('/login')
+def loginpage():
+    return redirect(url_for('login'))
+
+@app.route('/loginpage', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == admin_username and password == admin_password:
+        return redirect(url_for('dashboard', username=username)), 210
+
+    elif username == admin_username and password != admin_password:
+       return jsonify({'message': 'Incorrect Username. Please try again.', 'status_code': 404}), 404
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    username = request.args.get('username')
+    if username :
+        return render_template('dashboard.html', username=username), 200
+    else:
+        return jsonify({'message': 'You need to log in first.', 'status_code': 401}), 401
+
+@app.route('/logout')
+def logout():
+    username = request.args.get('username')
+    if username:
+        return jsonify({'message': 'You have been logged out.' , 'status_code': 200}), 200
+    else:
+        return jsonify({'message': 'You are not logged in.', 'status_code': 401}), 401
+    
+if __name__ == "__main__":
+    app.run(debug=True)
